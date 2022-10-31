f02c544158783e3abbc917a864cbd463bcae7f7f
soegaeni <szogyenyid@gmail.com>
2022-10-31 14:05:58 +0100

    Handle manual steps and variables in a pipeline

--- src/Pipeline.php
+++ src/Pipeline.php
@@ -2,17 +2,31 @@
 
 namespace Soegaeni\PhpCicdBuilder;
 
-final class Pipeline
+use Soegaeni\PhpCicdBuilder\Interfaces\Arrayable;
+
+final class Pipeline implements Arrayable
 {
     private $name;
     private $steps = array();
+    private $variables = array();
 
     public function __construct(string $name = "")
     {
         $this->name = $name;
     }
+    public function withVariables(Variables $variables): Pipeline
+    {
+        array_push($this->variables, $variables);
+        return $this;
+    }
     public function withStep(Step $step): Pipeline
     {
+        array_push($this->steps, $step);
+        return $this;
+    }
+    public function withManualStep(Step $step): Pipeline
+    {
+        $step = $step->withManualTrigger(true);
         array_push($this->steps, $step);
         return $this;
     }
@@ -27,6 +41,9 @@ final class Pipeline
     public function asArray(): array
     {
         $pipeline = array();
+        foreach ($this->variables as $var) {
+            array_push($pipeline, array("variables" => $var->asArray()));
+        }
         foreach ($this->steps as $step) {
             array_push($pipeline, array("step" => $step->asArray()));
         }
