596ee048a3696256ea1dc4fd657449e133abaafa
soegaeni <szogyenyid@gmail.com>
2022-10-31 12:51:48 +0100

    Dont use PHP without installing php

--- scripts/PHPUnit.php
+++ scripts/PHPUnit.php
@@ -8,6 +8,7 @@ class PHPUnit extends Script
 {
     public function __construct(string $path = "tests")
     {
+        $this->addInitCommand("apk add php8");
         $this->addInitCommand("wget https://phar.phpunit.de/phpunit-9.5.phar");
         $this->addCommand('php phpunit-9.5.phar --no-configuration --bootstrap vendor/autoload.php ' . $path);
     }
