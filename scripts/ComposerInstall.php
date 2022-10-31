4cbba41212a4e04826c64f978432a7b3b36ec9a3
soegaeni <szogyenyid@gmail.com>
2022-10-31 12:53:47 +0100

    Added Composer Install script

---
+++ scripts/ComposerInstall.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyProject\CICD;
+
+use Soegaeni\PhpCicdBuilder\Script;
+
+class ComposerInstall extends Script
+{
+    public function __construct()
+    {
+        $this->addInitCommand("apk add php8");
+        $this->addInitCommand('wget https://github.com/composer/composer/releases/download/2.4.4/composer.phar');
+        $this->addCommand('php composer.phar install');
+        $this->addCommand('echo "!/vendor/" >> .git-ftp-include;');
+    }
+}
