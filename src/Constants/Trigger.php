c7612828076d302e0136a26b62cacca58ff1a779
Daniel Szogyenyi <szogyenyid@gmail.com>
2022-11-30 13:22:17 +0100

    Add some other, unused trigger types

--- src/Constants/Trigger.php
+++ src/Constants/Trigger.php
@@ -4,7 +4,10 @@ namespace Soegaeni\PhpCicdBuilder\Constants;
 
 class Trigger
 {
+    public const DEFAULT = "default";
     public const BRANCH = "branches";
+    public const TAG = "tags";
+    public const BOOKMARK = "bookmarks";
     public const CUSTOM = "custom";
     public const PR = "pull-requests";
     public const MANUAL = "manual";
