dee624826cac45556c3951a7a34dea24287f5309
soegaeni <szogyenyid@gmail.com>
2022-12-12 13:39:54 +0100

    Add dependencies to PHPUnit script

--- scripts/PHPUnit.php
+++ scripts/PHPUnit.php
@@ -8,7 +8,7 @@ class PHPUnit extends Script
 {
     public function __construct(string $path = "tests")
     {
-        $this->addInitCommand("apk add php8");
+        $this->addInitCommand("apk add php8 php8-dom php8-json");
         $this->addInitCommand("wget https://phar.phpunit.de/phpunit-9.5.phar");
         $this->addCommand('php phpunit-9.5.phar --no-configuration --bootstrap vendor/autoload.php ' . $path);
     }
