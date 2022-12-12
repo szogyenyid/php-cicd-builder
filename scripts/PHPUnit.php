61846c7b7a93da8c87c8932feb485912363d2c10
soegaeni <szogyenyid@gmail.com>
2022-12-12 13:47:25 +0100

    Add the required xml extension

--- scripts/PHPUnit.php
+++ scripts/PHPUnit.php
@@ -8,7 +8,7 @@ class PHPUnit extends Script
 {
     public function __construct(string $path = "tests")
     {
-        $this->addInitCommand("apk add php8 php8-dom php8-json php8-mbstring php8-tokenizer");
+        $this->addInitCommand("apk add php8 php8-dom php8-json php8-mbstring php8-tokenizer php8-xml");
         $this->addInitCommand("wget https://phar.phpunit.de/phpunit-9.5.phar");
         $this->addCommand('php phpunit-9.5.phar --no-configuration --bootstrap vendor/autoload.php ' . $path);
     }
