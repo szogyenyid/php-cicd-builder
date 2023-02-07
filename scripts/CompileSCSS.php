3f6566185c8041331ea7fa2873d581181602651f
Daniel Szogyenyi <szogyenyid@gmail.com>
2023-02-07 14:18:39 +0100

    Added the ability to embed source map in compiled css files

--- scripts/CompileSCSS.php
+++ scripts/CompileSCSS.php
@@ -10,11 +10,12 @@ class CompileSCSS extends Script
         string $inputPath = "assets",
         string $outputPath = "public",
         bool $compress = true,
+        bool $embedSourceMap = true,
         bool $addToGitFtpInclude = true
     ) {
         $this->addInitCommand('apk add npm');
         $this->addInitCommand('npm install -g sass');
-        $this->addCommand('sass --update ' . $inputPath . ':' . $outputPath . ($compress ? ' --style=compressed' : ''));
+        $this->addCommand('sass --update ' . $inputPath . ':' . $outputPath . ($compress ? ' --style=compressed' : '') . ($embedSourceMap ? ' --embed-source-map' : ''));
         if ($addToGitFtpInclude) {
             $this->addCommand('echo "!/' . $outputPath . '/" >> .git-ftp-include;');
         }
